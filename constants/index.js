f7bd565b6269c47a8ee8590963c30519c9b68943
kevas007 <kevassiobo@gmail.com>
2022-08-03 10:30:31 +0200

    add navigation

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+
+import { COLORS, SHADOWS, SIZES, FONTS } from "./theme";
+
+export { assets, COLORS, SHADOWS, SIZES, FONTS, NFTData };
---
+++ screens/Home.js
@@ -0,0 +1,16 @@
+import { View, Text ,SafeAreaView, FlatList} from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+       <View style={{flex: 1}}>
+        <Text>
+            Home
+        </Text>
+       </View>
+    </SafeAreaView>
+  )
+}
+
+export default Home
